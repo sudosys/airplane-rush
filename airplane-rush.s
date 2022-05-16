cc9ebe7890d8fc3a4ea99b3fcabc3435643e273a
sudosys <67098654+sudosys@users.noreply.github.com>
2022-05-16 18:23:22 +0300

    draw_sky procedure added (fix addr out of range)

--- airplane-rush.s
+++ airplane-rush.s
@@ -1,1 +1,61 @@
 # Airplane Rush Game
+
+# Display Attributes
+.eqv WIDTH 512
+.eqv HEIGTH 256
+.eqv ORIGIN 0x10010000
+
+# Colors
+.eqv CYAN 0x0000FFFF # Sky
+.eqv BLUE 0x000000FF # Plane
+.eqv YELLOW 0x00FFFF00 # Birds
+.eqv RED 0x00FF0000 # Fuel tanks
+
+.text
+
+main:
+
+	# call draw_sky
+	jal draw_sky
+	
+	li $v0, 10
+	syscall
+
+draw_sky:
+	addiu $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	addi $t3, $zero, CYAN
+	addi $t4, $zero, WIDTH
+	addi $t5, $zero, HEIGTH
+
+	addi $a0, $zero, 0
+	addi $a1, $zero, 0
+	jal calc_pixel_addr
+	
+	draw_loop:
+		sw $t3, 0($v0)
+		subi $t4, $t4, 1
+		addiu $v0, $v0, 4
+		bnez $t4, draw_loop
+	
+	subi $t5, $t5, 1
+	bnez $t5, draw_loop
+		
+	lw $ra, 0($sp)
+	addiu, $sp, $sp, 4
+	
+	jr $ra
+
+# Parameters: $a0 = x_coordinate, $a1 = y_coordinate
+calc_pixel_addr:
+	
+	# Formula: ORIGIN + 4 * (x_coordinate + y_coordinate * WIDTH)
+
+	addiu $t0, $zero, WIDTH
+	mul $t1, $t0, $a1
+	addu $t2, $t1, $a0
+	sll $t2, $t2, 2
+	addiu $v0, $t2, ORIGIN
+	
+	jr $ra
